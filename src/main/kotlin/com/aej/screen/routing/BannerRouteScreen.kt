5a0e00ca9e9ca4a4a91293b9fe5782f418ec0fea
utsman_ <muhammad.utsman@ruangguru.com>
2022-03-01 00:11:16 +0700

    Fix add banner

--- src/main/kotlin/com/aej/screen/routing/BannerRouteScreen.kt
+++ src/main/kotlin/com/aej/screen/routing/BannerRouteScreen.kt
@@ -6,8 +6,12 @@ import com.aej.repository.banner.Banner
 import com.aej.repository.user.User
 import com.aej.screen.request.BannerRequest
 import com.aej.screen.response.MainResponse
+import com.aej.services.image.ImageStorageServices
 import com.aej.utils.mapToResponse
+import com.aej.utils.orNol
+import com.aej.utils.orRandom
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -15,15 +19,33 @@ import io.ktor.server.response.*
 object BannerRouteScreen {
     private val userRepository = KoinContainer.userRepository
     private val bannerRepository = KoinContainer.bannerRepository
+    private val valueContainer = KoinContainer.valueContainer
 
     suspend fun createBanner(applicationCall: ApplicationCall) = applicationCall.run {
-        val user = User.fromToken(request, userRepository)
-        val request = receive<BannerRequest>()
+        val user = User.fromToken(request, userRepository, User.Role.SELLER)
+        val banner = Banner(sellerId = user.id)
 
-        if (user.role == User.Role.CUSTOMER) throw MainException("Role not permission", HttpStatusCode.BadRequest)
-        if (request == BannerRequest()) throw MainException("Banner is empty", HttpStatusCode.BadRequest)
+        receiveMultipart().readAllParts().onEach { part ->
+            when (part) {
+                is PartData.FormItem -> {
+                    when (part.name) {
+                        "name" -> banner.name = part.value
+                        "headline" -> banner.headline = part.value
+                        "caption" -> banner.caption = part.value
+                        "product_id" -> banner.productId = part.value
+                    }
+                }
+                is PartData.FileItem -> {
+                    val fileBytes = part.streamProvider().readBytes()
+                    val baseUrl = valueContainer.getBaseUrl()
 
-        val banner = Banner.of(request, user.id)
+                    val urlImage = ImageStorageServices.uploadFile(fileBytes, part.originalFileName.orRandom(), user.username, baseUrl)
+                    banner.imageUrl = urlImage
+                }
+                else -> {}
+            }
+        }
+
         bannerRepository.createBanner(banner)
         val bannerDb = bannerRepository.getBannerId(banner.id)
         val bannerResponse = bannerDb.mapToResponse()
